828f4d12d0bcd57d9d8e6f5f87e6492a21d11e0d
MWK <sunnybikers@naver.com>
2025-10-09 23:19:17 +0900

    Revert "feat: MEM-01 #12 개발"

--- CC_BE/src/main/java/com/capstone/web/member/dto/MemberRegisterResponse.java
+++
@@ -1,8 +0,0 @@
-package com.capstone.web.member.dto;
-
-public record MemberRegisterResponse(Long id, String email, String nickname, String message) {
-
-    public static MemberRegisterResponse of(Long id, String email, String nickname) {
-        return new MemberRegisterResponse(id, email, nickname, "회원가입이 완료되었습니다.");
-    }
-}
