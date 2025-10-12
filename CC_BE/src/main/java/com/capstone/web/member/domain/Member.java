3b080ea495c203965011f2805257f2c517ccc3f3
MWK <sunnybikers@naver.com>
2025-10-13 01:18:29 +0900

    fix: update table name from 'users' to 'members' in Member entity

--- CC_BE/src/main/java/com/capstone/web/member/domain/Member.java
+++ CC_BE/src/main/java/com/capstone/web/member/domain/Member.java
@@ -11,7 +11,7 @@ import java.time.LocalDateTime;
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Entity
-@Table(name = "users")
+@Table(name = "members")
 @EntityListeners(AuditingEntityListener.class)
 public class Member {
     @Id
