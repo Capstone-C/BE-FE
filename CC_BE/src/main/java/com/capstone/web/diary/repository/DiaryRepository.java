11ba0a58277dd09216034b0ddb991211f70676e6
Kim JaeHoon <145709439+dowoo108380@users.noreply.github.com>
2025-11-06 19:12:08 -0800

    bugfix: conflict 과정 중 생긴 버그 수정. (#149)

--- CC_BE/src/main/java/com/capstone/web/diary/repository/DiaryRepository.java
+++ CC_BE/src/main/java/com/capstone/web/diary/repository/DiaryRepository.java
@@ -19,31 +19,31 @@ public interface DiaryRepository extends JpaRepository<Diary, Long> {
      * 특정 회원의 특정 연월에 해당하는 모든 식단 기록 조회
      */
     @Query("SELECT d FROM Diary d WHERE d.member = :member " +
-           "AND YEAR(d.date) = :year AND MONTH(d.date) = :month " +
-           "ORDER BY d.date ASC, " +
-           "CASE d.mealType " +
-           "WHEN com.capstone.web.diary.domain.Diary$MealType.BREAKFAST THEN 0 " +
-           "WHEN com.capstone.web.diary.domain.Diary$MealType.LUNCH THEN 1 " +
-           "WHEN com.capstone.web.diary.domain.Diary$MealType.DINNER THEN 2 " +
-           "WHEN com.capstone.web.diary.domain.Diary$MealType.SNACK THEN 3 " +
-           "END ASC")
+            "AND YEAR(d.date) = :year AND MONTH(d.date) = :month " +
+            "ORDER BY d.date ASC, " +
+            "CASE d.mealType " +
+            "WHEN com.capstone.web.diary.domain.Diary.MealType.BREAKFAST THEN 0 " +
+            "WHEN com.capstone.web.diary.domain.Diary.MealType.LUNCH THEN 1 " +
+            "WHEN com.capstone.web.diary.domain.Diary.MealType.DINNER THEN 2 " +
+            "WHEN com.capstone.web.diary.domain.Diary.MealType.SNACK THEN 3 " +
+            "END ASC")
     List<Diary> findByMemberAndYearMonth(@Param("member") Member member,
-                                          @Param("year") int year,
-                                          @Param("month") int month);
+                                         @Param("year") int year,
+                                         @Param("month") int month);
 
     /**
      * 특정 회원의 특정 날짜에 해당하는 모든 식단 기록 조회
      */
     @Query("SELECT d FROM Diary d WHERE d.member = :member AND d.date = :date " +
-           "ORDER BY " +
-           "CASE d.mealType " +
-           "WHEN com.capstone.web.diary.domain.Diary$MealType.BREAKFAST THEN 0 " +
-           "WHEN com.capstone.web.diary.domain.Diary$MealType.LUNCH THEN 1 " +
-           "WHEN com.capstone.web.diary.domain.Diary$MealType.DINNER THEN 2 " +
-           "WHEN com.capstone.web.diary.domain.Diary$MealType.SNACK THEN 3 " +
-           "END ASC")
+            "ORDER BY " +
+            "CASE d.mealType " +
+            "WHEN com.capstone.web.diary.domain.Diary.MealType.BREAKFAST THEN 0 " +
+            "WHEN com.capstone.web.diary.domain.Diary.MealType.LUNCH THEN 1 " +
+            "WHEN com.capstone.web.diary.domain.Diary.MealType.DINNER THEN 2 " +
+            "WHEN com.capstone.web.diary.domain.Diary.MealType.SNACK THEN 3 " +
+            "END ASC")
     List<Diary> findByMemberAndDateOrderByMealTypeAsc(@Param("member") Member member,
-                                                       @Param("date") LocalDate date);
+                                                      @Param("date") LocalDate date);
 
     /**
      * 특정 회원의 특정 날짜, 특정 식사 타입에 기록이 있는지 확인
